785fb446f0013da5bb7282fcb7ba40528081403a
KaiSej007 <kailyubo@gmail.com>
2023-11-13 10:45:46 +0100

    hej

---
+++ src/main/java/com/example/httpsdemo/HttpSdemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.httpsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSdemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HttpSdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/httpsdemo/HttpSdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.httpsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpSdemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
